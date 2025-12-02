005e53a49d11963a4a51f2f3584cf217724b724f
Cullen Daugherty <dakjaniels@outlook.com>
2025-12-01 23:24:32 -0600

    luals

--- LuiExtended/pc/Unlock.lua
+++ LuiExtended/pc/Unlock.lua
@@ -1,4 +1,4 @@
---- @diagnostic disable: duplicate-set-field
+--- @diagnostic disable: duplicate-set-field, duplicate-doc-field
 -- -----------------------------------------------------------------------------
 --  LuiExtended                                                               --
 --  Distributed under The MIT License (MIT) (see LICENSE file)                --
