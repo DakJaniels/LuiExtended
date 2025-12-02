afea8f957a666171f16f705d0ec992a608a173dd
Cullen Daugherty <dakjaniels@outlook.com>
2025-12-01 23:23:39 -0600

    fix: synergy tracker

---
+++ .meta/SynergyTracker_Definitions.lua
@@ -0,0 +1,350 @@
+-- ////// START : GENERATED FROM C:/Users/dack_janiels/source/repos/LUIE/LuiExtended/LuiExtended/frontend\SynergyTracker.xml
+---------- LVL: 00 ----------
+---------- LVL: 01 ----------
+---------- LVL: 02 ----------
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI : TopLevelWindow
+LUIE_SynergyTracker_UI = ...
+---------- LVL: 03 ----------
+---------- LVL: 04 ----------
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Background : BackdropControl
+LUIE_SynergyTracker_UI_Background = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row1 : Control
+LUIE_SynergyTracker_UI_Row1 = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row2 : Control
+LUIE_SynergyTracker_UI_Row2 = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row3 : Control
+LUIE_SynergyTracker_UI_Row3 = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row4 : Control
+LUIE_SynergyTracker_UI_Row4 = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row5 : Control
+LUIE_SynergyTracker_UI_Row5 = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row6 : Control
+LUIE_SynergyTracker_UI_Row6 = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row7 : Control
+LUIE_SynergyTracker_UI_Row7 = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row8 : Control
+LUIE_SynergyTracker_UI_Row8 = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row9 : Control
+LUIE_SynergyTracker_UI_Row9 = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row10 : Control
+LUIE_SynergyTracker_UI_Row10 = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Preview : BackdropControl
+LUIE_SynergyTracker_UI_Preview = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Preview_CoordLabel : LabelControl
+LUIE_SynergyTracker_UI_Preview_CoordLabel = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Preview_Label : LabelControl
+LUIE_SynergyTracker_UI_Preview_Label = ...
+---------- LVL: 05 ----------
+---------- LVL: 06 ----------
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row1_IconBg : TextureControl
+LUIE_SynergyTracker_UI_Row1_IconBg = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row1_Icon : TextureControl
+LUIE_SynergyTracker_UI_Row1_Icon = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row1_PosNum : LabelControl
+LUIE_SynergyTracker_UI_Row1_PosNum = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row1_Name : LabelControl
+LUIE_SynergyTracker_UI_Row1_Name = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row1_Priority : LabelControl
+LUIE_SynergyTracker_UI_Row1_Priority = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row1_Cooldown : CooldownControl
+LUIE_SynergyTracker_UI_Row1_Cooldown = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row1_CooldownText : LabelControl
+LUIE_SynergyTracker_UI_Row1_CooldownText = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row2_IconBg : TextureControl
+LUIE_SynergyTracker_UI_Row2_IconBg = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row2_Icon : TextureControl
+LUIE_SynergyTracker_UI_Row2_Icon = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row2_PosNum : LabelControl
+LUIE_SynergyTracker_UI_Row2_PosNum = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row2_Name : LabelControl
+LUIE_SynergyTracker_UI_Row2_Name = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row2_Priority : LabelControl
+LUIE_SynergyTracker_UI_Row2_Priority = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row2_Cooldown : CooldownControl
+LUIE_SynergyTracker_UI_Row2_Cooldown = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row2_CooldownText : LabelControl
+LUIE_SynergyTracker_UI_Row2_CooldownText = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row3_IconBg : TextureControl
+LUIE_SynergyTracker_UI_Row3_IconBg = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row3_Icon : TextureControl
+LUIE_SynergyTracker_UI_Row3_Icon = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row3_PosNum : LabelControl
+LUIE_SynergyTracker_UI_Row3_PosNum = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row3_Name : LabelControl
+LUIE_SynergyTracker_UI_Row3_Name = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row3_Priority : LabelControl
+LUIE_SynergyTracker_UI_Row3_Priority = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row3_Cooldown : CooldownControl
+LUIE_SynergyTracker_UI_Row3_Cooldown = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row3_CooldownText : LabelControl
+LUIE_SynergyTracker_UI_Row3_CooldownText = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row4_IconBg : TextureControl
+LUIE_SynergyTracker_UI_Row4_IconBg = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row4_Icon : TextureControl
+LUIE_SynergyTracker_UI_Row4_Icon = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row4_PosNum : LabelControl
+LUIE_SynergyTracker_UI_Row4_PosNum = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row4_Name : LabelControl
+LUIE_SynergyTracker_UI_Row4_Name = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row4_Priority : LabelControl
+LUIE_SynergyTracker_UI_Row4_Priority = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row4_Cooldown : CooldownControl
+LUIE_SynergyTracker_UI_Row4_Cooldown = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row4_CooldownText : LabelControl
+LUIE_SynergyTracker_UI_Row4_CooldownText = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row5_IconBg : TextureControl
+LUIE_SynergyTracker_UI_Row5_IconBg = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row5_Icon : TextureControl
+LUIE_SynergyTracker_UI_Row5_Icon = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row5_PosNum : LabelControl
+LUIE_SynergyTracker_UI_Row5_PosNum = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row5_Name : LabelControl
+LUIE_SynergyTracker_UI_Row5_Name = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row5_Priority : LabelControl
+LUIE_SynergyTracker_UI_Row5_Priority = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row5_Cooldown : CooldownControl
+LUIE_SynergyTracker_UI_Row5_Cooldown = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row5_CooldownText : LabelControl
+LUIE_SynergyTracker_UI_Row5_CooldownText = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row6_IconBg : TextureControl
+LUIE_SynergyTracker_UI_Row6_IconBg = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row6_Icon : TextureControl
+LUIE_SynergyTracker_UI_Row6_Icon = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row6_PosNum : LabelControl
+LUIE_SynergyTracker_UI_Row6_PosNum = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row6_Name : LabelControl
+LUIE_SynergyTracker_UI_Row6_Name = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row6_Priority : LabelControl
+LUIE_SynergyTracker_UI_Row6_Priority = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row6_Cooldown : CooldownControl
+LUIE_SynergyTracker_UI_Row6_Cooldown = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row6_CooldownText : LabelControl
+LUIE_SynergyTracker_UI_Row6_CooldownText = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row7_IconBg : TextureControl
+LUIE_SynergyTracker_UI_Row7_IconBg = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row7_Icon : TextureControl
+LUIE_SynergyTracker_UI_Row7_Icon = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row7_PosNum : LabelControl
+LUIE_SynergyTracker_UI_Row7_PosNum = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row7_Name : LabelControl
+LUIE_SynergyTracker_UI_Row7_Name = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row7_Priority : LabelControl
+LUIE_SynergyTracker_UI_Row7_Priority = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row7_Cooldown : CooldownControl
+LUIE_SynergyTracker_UI_Row7_Cooldown = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row7_CooldownText : LabelControl
+LUIE_SynergyTracker_UI_Row7_CooldownText = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row8_IconBg : TextureControl
+LUIE_SynergyTracker_UI_Row8_IconBg = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row8_Icon : TextureControl
+LUIE_SynergyTracker_UI_Row8_Icon = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row8_PosNum : LabelControl
+LUIE_SynergyTracker_UI_Row8_PosNum = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row8_Name : LabelControl
+LUIE_SynergyTracker_UI_Row8_Name = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row8_Priority : LabelControl
+LUIE_SynergyTracker_UI_Row8_Priority = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row8_Cooldown : CooldownControl
+LUIE_SynergyTracker_UI_Row8_Cooldown = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row8_CooldownText : LabelControl
+LUIE_SynergyTracker_UI_Row8_CooldownText = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row9_IconBg : TextureControl
+LUIE_SynergyTracker_UI_Row9_IconBg = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row9_Icon : TextureControl
+LUIE_SynergyTracker_UI_Row9_Icon = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row9_PosNum : LabelControl
+LUIE_SynergyTracker_UI_Row9_PosNum = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row9_Name : LabelControl
+LUIE_SynergyTracker_UI_Row9_Name = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row9_Priority : LabelControl
+LUIE_SynergyTracker_UI_Row9_Priority = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row9_Cooldown : CooldownControl
+LUIE_SynergyTracker_UI_Row9_Cooldown = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row9_CooldownText : LabelControl
+LUIE_SynergyTracker_UI_Row9_CooldownText = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row10_IconBg : TextureControl
+LUIE_SynergyTracker_UI_Row10_IconBg = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row10_Icon : TextureControl
+LUIE_SynergyTracker_UI_Row10_Icon = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row10_PosNum : LabelControl
+LUIE_SynergyTracker_UI_Row10_PosNum = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row10_Name : LabelControl
+LUIE_SynergyTracker_UI_Row10_Name = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row10_Priority : LabelControl
+LUIE_SynergyTracker_UI_Row10_Priority = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row10_Cooldown : CooldownControl
+LUIE_SynergyTracker_UI_Row10_Cooldown = ...
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+---@class LUIE_SynergyTracker_UI_Row10_CooldownText : LabelControl
+LUIE_SynergyTracker_UI_Row10_CooldownText = ...
+---------- LVL: 07 ----------
+-- ////// END   : GENERATED FROM C:/Users/dack_janiels/source/repos/LUIE/LuiExtended/LuiExtended/frontend\SynergyTracker.xml
