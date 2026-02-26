5836c641748e9b6ec877b367cd79d464c2afb9a4
Cullen Daugherty <dakjaniels@outlook.com>
2026-02-26 06:32:35 -0600

    inside the if

--- LuiExtended/src/LuiExtended.lua
+++ LuiExtended/src/LuiExtended.lua
@@ -226,19 +226,18 @@ end
 
 do
     local g_loggingEnabled = LUIE.IsDevDebugEnabled()
-    if not g_loggingEnabled then
-        return
-    end
-    local function ZO_Scene_Log(self, message)
-        LUIE:Log("Verbose", string.format("%s - %s - %s", GetString("SI_SCENEMANAGERMESSAGEORIGIN", ZO_REMOTE_SCENE_CHANGE_ORIGIN), self.name, message))
-    end
-    ZO_Scene.Log = ZO_Scene_Log
-    local function ZO_SceneManager_Follower_Log(self, message, sceneName)
-        if sceneName then
-            LUIE:Log("Verbose", string.format("%s - %s - %s", GetString("SI_SCENEMANAGERMESSAGEORIGIN", ZO_REMOTE_SCENE_CHANGE_ORIGIN), message, sceneName))
-        else
-            LUIE:Log("Verbose", string.format("%s - %s", GetString("SI_SCENEMANAGERMESSAGEORIGIN", ZO_REMOTE_SCENE_CHANGE_ORIGIN), message))
+    if g_loggingEnabled then
+        local function ZO_Scene_Log(self, message)
+            LUIE:Log("Verbose", string.format("%s - %s - %s", GetString("SI_SCENEMANAGERMESSAGEORIGIN", ZO_REMOTE_SCENE_CHANGE_ORIGIN), self.name, message))
         end
+        ZO_Scene.Log = ZO_Scene_Log
+        local function ZO_SceneManager_Follower_Log(self, message, sceneName)
+            if sceneName then
+                LUIE:Log("Verbose", string.format("%s - %s - %s", GetString("SI_SCENEMANAGERMESSAGEORIGIN", ZO_REMOTE_SCENE_CHANGE_ORIGIN), message, sceneName))
+            else
+                LUIE:Log("Verbose", string.format("%s - %s", GetString("SI_SCENEMANAGERMESSAGEORIGIN", ZO_REMOTE_SCENE_CHANGE_ORIGIN), message))
+            end
+        end
+        ZO_SceneManager_Follower.Log = ZO_SceneManager_Follower_Log
     end
-    ZO_SceneManager_Follower.Log = ZO_SceneManager_Follower_Log
 end
