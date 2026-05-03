82fda79501ef576c76f6192cc014b5c616de1dc9
Cullen Daugherty <dakjaniels@outlook.com>
2026-05-02 19:07:42 -0500

    fix: Update Unlock functionality for Night Market favor counter; ensure frame mover dimensions match actual content and prevent fullscreen stretch on first unlock

--- LuiExtended/src/LuiExtended.lua
+++ LuiExtended/src/LuiExtended.lua
@@ -28,8 +28,8 @@ local LUIE = LUIE
 -- -----------------------------------------------------------------------------
 LUIE.tag = "LUIE"
 LUIE.name = "LuiExtended"
-LUIE.version = "7.2.1.0"
-LUIE.addonVersion = 7210
+LUIE.version = "7.2.1.1"
+LUIE.addonVersion = 7211
 LUIE.author = "@dack_janiels[PC]"
 LUIE.legacyAuthors = "ArtOfShred, psypanda, Saenic & SpellBuilder"
 LUIE.website = "https://www.esoui.com/downloads/info818-LuiExtended.html"
