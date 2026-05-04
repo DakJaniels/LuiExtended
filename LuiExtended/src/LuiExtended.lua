d45006597514cb284fae1e948db6ea35c7b2944f
Cullen Daugherty <dakjaniels@outlook.com>
2026-05-04 01:42:04 -0500

    fix: Bump version to 7.2.1.2 and address nil value error in UnitFrames module

--- LuiExtended/src/LuiExtended.lua
+++ LuiExtended/src/LuiExtended.lua
@@ -28,8 +28,8 @@ local LUIE = LUIE
 -- -----------------------------------------------------------------------------
 LUIE.tag = "LUIE"
 LUIE.name = "LuiExtended"
-LUIE.version = "7.2.1.1"
-LUIE.addonVersion = 7211
+LUIE.version = "7.2.1.2"
+LUIE.addonVersion = 7212
 LUIE.author = "@dack_janiels[PC]"
 LUIE.legacyAuthors = "ArtOfShred, psypanda, Saenic & SpellBuilder"
 LUIE.website = "https://www.esoui.com/downloads/info818-LuiExtended.html"
