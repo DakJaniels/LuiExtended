b10d7298bae2bfb577d84d2df6d947b4405c479d
Cullen Daugherty <dakjaniels@outlook.com>
2026-05-25 17:03:20 -0500

    refactor: streamline Unit Frames XML and Lua integration by replacing static control creation with virtual templates, enhancing layout flexibility and reducing redundancy

--- LuiExtended/src/LuiExtended.lua
+++ LuiExtended/src/LuiExtended.lua
@@ -329,8 +329,8 @@ local DEVS = readonlytable
         },
         ["@dack_janiels"] =
         {
-            enabled = false,
-            debug = false,
+            enabled = true,
+            debug = true,
         },
         ["@dack_janiels.luie"] =
         {
