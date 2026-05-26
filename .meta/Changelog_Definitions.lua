0c6efe1bf1a3ffac548bf4717146f25d07ff0553
Cullen Daugherty <dakjaniels@outlook.com>
2026-05-26 00:10:47 -0500

    chore: update generated docs

--- .meta/Changelog_Definitions.lua
+++ .meta/Changelog_Definitions.lua
@@ -1,4 +1,4 @@
--- ////// START : GENERATED FROM C:\Users\dack_janiels\Documents\LUIE\LuiExtended\LuiExtended\frontend\Changelog.xml
+-- ////// START : GENERATED FROM C:\Users\dack_janiels\Documents\LUIE\LuiExtended\LuiExtended\pc\frontend\Changelog.xml
 ---------- LVL: 00 ----------
 ---------- LVL: 01 ----------
 ---------- LVL: 02 ----------
@@ -86,4 +86,4 @@ LUIE_Changelog_Title = {}
 ---@field public OnClicked fun(self: Control, button: integer, ctrl: boolean, alt: boolean, shift: boolean, command: boolean)
 LUIE_Changelog_Close = {}
 ---------- LVL: 07 ----------
--- ////// END   : GENERATED FROM C:\Users\dack_janiels\Documents\LUIE\LuiExtended\LuiExtended\frontend\Changelog.xml
+-- ////// END   : GENERATED FROM C:\Users\dack_janiels\Documents\LUIE\LuiExtended\LuiExtended\pc\frontend\Changelog.xml
