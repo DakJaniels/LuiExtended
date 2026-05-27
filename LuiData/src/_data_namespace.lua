7cb66162fd778845e7177d4e9572d1a2d03fa190
Cullen Daugherty <dakjaniels@outlook.com>
2026-05-27 14:04:59 -0500

    Revert "chore: bump LuiData version to 7.2.1.2"
    This reverts commit 51bcd77a5ab1090606b18fc746c3ad89e6840aea.

--- LuiData/src/_data_namespace.lua
+++ LuiData/src/_data_namespace.lua
@@ -251,8 +251,8 @@ local ZoneTable = {}
 --- @class (partial) LuiData
 LuiData = {}
 LuiData.name = "LuiData"
-LuiData.version = 7212
-LuiData.addonVersion = "7.2.1.2"
+LuiData.version = 7211
+LuiData.addonVersion = "7.2.1.1"
 
 --- @class (partial) Data
 LuiData.Data =
