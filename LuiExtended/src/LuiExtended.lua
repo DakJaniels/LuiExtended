9f69ec2ece1ee71884c1db4f8964d34ac3be91fb
Cullen Daugherty <dakjaniels@outlook.com>
2026-06-10 03:03:44 -0500

    debug

--- LuiExtended/src/LuiExtended.lua
+++ LuiExtended/src/LuiExtended.lua
@@ -384,8 +384,8 @@ local DEVS = readonlytable
         },
         ["@dack_janiels"] =
         {
-            enabled = false,
-            debug = false,
+            enabled = true,
+            debug = true,
         },
         ["@dack_janiels.luie"] =
         {
